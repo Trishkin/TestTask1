10d5d6de5199a41c2acf98b25391c21ae03732d1
“Kulikov <dima.dmitry.kulikov@gmail.com>
2024-05-24 20:31:05 +0300

    first commit

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public OrderService(ApplicationDbContext applicationDbContext) 
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public Task<Order> GetOrder()
+        {
+            return _applicationDbContext.Orders.Where(x => x.Quantity > 1)
+                .OrderByDescending(x => x.CreatedAt).FirstOrDefaultAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _applicationDbContext.Orders.Where(x => x.User.Status == Enums.UserStatus.Active)
+                .OrderByDescending(x => x.CreatedAt).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public UserService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public async Task<User> GetUser()
+        {
+            return await _applicationDbContext.Users
+                .Join(_applicationDbContext.Orders,
+                user => user.Id,
+                order => order.UserId,
+                (user, order) => new { User = user, Order = order })
+                .Where(joined => joined.Order.CreatedAt.Year == 2003)
+                .GroupBy(joined => joined.User)
+                .Select(grouped => new {
+                    User = grouped.Key,
+                    TotalAmount = grouped.Sum(joined => joined.Order.Price * joined.Order.Quantity)
+                })
+                .OrderByDescending(grouped => grouped.TotalAmount)
+                .Select(grouped => grouped.User)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _applicationDbContext.Users.Where(x => x.Orders.Any(x => x.CreatedAt.Year == 2010 &&
+            x.Status == Enums.OrderStatus.Paid)).ToListAsync();
+        }
+    }
+    
+}
