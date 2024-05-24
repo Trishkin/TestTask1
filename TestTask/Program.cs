fc01d92568c5bd29cedde954c31521e05c906cf7
“Kulikov <dima.dmitry.kulikov@gmail.com>
2024-05-24 23:01:18 +0300

    Change GetUser

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using TestTask.Data;
 using TestTask.Services.Implementations;
 using TestTask.Services.Interfaces;
@@ -7,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- TestTask/Services/Implementations/UserService.cs
+++ TestTask/Services/Implementations/UserService.cs
@@ -15,26 +15,16 @@ namespace TestTask.Services.Implementations
         }
         public async Task<User> GetUser()
         {
-            return await _applicationDbContext.Users
-                .Join(_applicationDbContext.Orders,
-                user => user.Id,
-                order => order.UserId,
-                (user, order) => new { User = user, Order = order })
-                .Where(joined => joined.Order.CreatedAt.Year == 2003)
-                .GroupBy(joined => joined.User)
-                .Select(grouped => new {
-                    User = grouped.Key,
-                    TotalAmount = grouped.Sum(joined => joined.Order.Price * joined.Order.Quantity)
-                })
-                .OrderByDescending(grouped => grouped.TotalAmount)
-                .Select(grouped => grouped.User)
-                .FirstOrDefaultAsync();
+            return _applicationDbContext.Orders.Where(x => x.CreatedAt.Year == 2003)
+                .GroupBy(x => x.User).ToList()
+                .OrderByDescending(x => x.Sum(s => s.Quantity * s.Price))
+                .FirstOrDefault().Key;
         }
 
         public Task<List<User>> GetUsers()
         {
-            return _applicationDbContext.Users.Where(x => x.Orders.Any(x => x.CreatedAt.Year == 2010 &&
-            x.Status == Enums.OrderStatus.Paid)).ToListAsync();
+            return _applicationDbContext.Users.Where(x => x.Orders.Any(o => o.CreatedAt.Year == 2010 &&
+            o.Status == Enums.OrderStatus.Paid)).ToListAsync();
         }
     }
     
